a7b7444cbcc6d6f9d065ade7895cb51968945ef3
Christian Brevik <christian.brevik@gmail.com>
2019-02-01 15:12:29 +0100

    Refactor to less nested if-statements
    To nested match-statements! This can't possibly be
    idiomatic Rust?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![feature(bind_by_move_pattern_guards)]
+
 use myhttp::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -25,50 +27,61 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let mut response = get_404_response();
-
     let request = String::from_utf8_lossy(&buffer[..]);
 
-    let mut lines = request.lines();
-    if let Some(res) = lines.next() {
-        let mut splits = res.split(" ");
-        splits.next();
-        if let Some(url) = splits.next() {
-            let formatted_path = format!(".{}", url);
-            let path = Path::new(&formatted_path);
-            if path.exists() {
-                if let Ok(metadata) = path.metadata() {
-                    if metadata.is_dir() {
-                        if let Ok(mut dir) = path.read_dir() {
-                            let has_index = dir.any(|entry| {
-                                if let Ok(e) = entry {
-                                    e.file_name() == "index.html"
-                                } else {
-                                    false
-                                }
-                            });
+    let response = get_response(String::from(request));
 
-                            if has_index {
-                                let file_path = format!("{}/index.html", formatted_path);
-                                let contents = fs::read_to_string(file_path).unwrap();
-                                response = get_200_response(contents);
-                            } else {
-                                response = get_200_response(get_dir_html(formatted_path));
-                            }
-                        } else {
-                            response = get_401_response();
-                        }
-                    } else {
-                        let contents = fs::read_to_string(formatted_path).unwrap();
-                        response = get_200_response(contents);
-                    }
-                }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn get_response(request: String) -> String {
+    match get_path_from_request(request) {
+        Some(path) if path.exists() => match path.metadata() {
+            Ok(metadata) if metadata.is_dir() => match path.read_dir() {
+                Ok(dir) => match find_index_file(dir) {
+                    Some(index_file) => match fs::read_to_string(index_file) {
+                        Ok(contents) => get_200_response(contents),
+                        _ => get_401_response(),
+                    },
+                    None => get_dir_response(path.read_dir().unwrap()),
+                },
+                Err(_) => get_401_response(),
+            },
+            Ok(metadata) if metadata.is_file() => match fs::read_to_string(path) {
+                Ok(contents) => get_200_response(contents),
+                _ => get_401_response(),
+            },
+            _ => get_401_response(),
+        },
+        _ => get_404_response(),
+    }
+}
+
+fn get_path_from_request(request: String) -> Option<PathBuf> {
+    if let Some(res) = request.lines().nth(0) {
+        let mut splits = res.split(" ");
+        if let Some(url) = splits.nth(1) {
+            let formatted = format!(".{}", url);
+            let path = Path::new(&formatted);
+            return Some(path.to_owned());
+        }
+    }
+
+    None
+}
+
+fn find_index_file(entries: fs::ReadDir) -> Option<PathBuf> {
+    for entry in entries {
+        if let Ok(e) = entry {
+            let file_name = e.file_name();
+            if file_name == "index.html" || file_name == "index.htm" {
+                return Some(e.path());
             }
         }
     }
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    None
 }
 
 fn get_200_response(contents: String) -> String {
@@ -84,9 +97,21 @@ fn get_404_response() -> String {
     String::from("HTTP/1.1 404 NOT FOUND\r\n\r\n404 Not Found")
 }
 
-fn get_dir_html(path: String) -> String {
-    for entry in fs::read_dir(path) {
-        println!("{:?}", entry);
-    }
-    format!("<html>{}</html>", "0")
+fn get_dir_response(entries: fs::ReadDir) -> String {
+    let entry_html = entries
+        .map(|e| e.unwrap())
+        .fold(String::new(), |prev, dir_entry| {
+            let path = dir_entry.path();
+            let result = prev
+                + &format!(
+                    "<li><a href=\"/{}\">{}</a></li>",
+                    path.strip_prefix(".").unwrap().to_str().unwrap(),
+                    path.file_name().unwrap().to_str().unwrap()
+                );
+            result
+        });
+    format!(
+        "HTTP/1.1 200 OK\r\n\r\n<html><body><ul>{}</ul></body></html>",
+        entry_html
+    )
 }
